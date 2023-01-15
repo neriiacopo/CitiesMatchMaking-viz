b2cb830ef979de878072ca2284ebfc4314e6f8d5
neriiacopo <neri.iacopo@gmail.com>
2023-01-15 22:12:47 +0100

    json upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import { OrbitControls } from "@react-three/drei";
-import Points from "./Points.js";
+import { useControls, buttonGroup, Leva } from "leva";
+import { useEffect, useState } from "react";
+import PointsSprite from "./PointsSprite.js";
+import PointsFrame from "./PointsFrame.js";
 
 export default function App() {
+    // Settings for GUI
+    const layer = useControls({
+        layer: {
+            value: "built",
+            options: {
+                built: "built",
+                landform: "landform",
+                NDVI: "NDVI",
+                landuse: "landuse",
+                "soil water": "soil water",
+                "night time light": "night lights",
+            },
+        },
+    });
+    const size = useControls("", {
+        size: {
+            value: 1,
+            min: 0,
+            max: 2,
+        },
+    });
+    const label = useControls("", { label: false });
+
     return (
         <>
-            <OrbitControls
-                makeDefault //make the default control so that other helps will deactive it if necessary (GIZMO conflict)
-                enableDamping={false}
-            />
-
-            <directionalLight
+            <OrbitControls />
+            {/* <directionalLight
                 position={[1, 2, 3]}
                 intensity={1.5}
             />
-            <ambientLight intensity={0.5} />
-
-            {/* <mesh scale={0.1}>
-                <sphereGeometry />
-                <meshBasicMaterial />
-            </mesh> */}
-
-            <Points />
+            <ambientLight intensity={0.5} /> */}
+            <PointsSprite
+                layer={layer.layer}
+                size={size.size}
+                label={label.label}
+            />
+            {/* <PointsFrame
+                layer={layer.layer}
+                size={size.size}
+                label={label.label}
+            /> */}
         </>
     );
 }
